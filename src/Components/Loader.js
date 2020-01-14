c211d66d660b59e62b59e86580dc0b93949f6eaa
seunghyun <seunghyunleekr@gmail.com>
2020-01-14 17:56:11 +0900

    add post & feed

---
+++ src/Components/Loader.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { Logo } from "./Icons";
+
+const Animation = keyframes`
+    0%{
+        opacity:0
+    }
+    50%{
+        opacity:50
+    }
+    100%{
+        opacity:100
+    }
+`;
+
+const Loader = styled.div`
+    animation: ${Animation} 1s linear infinite;
+`;
+
+export default () => (
+    <Loader>
+        <Logo size={36} />
+    </Loader>
+);
