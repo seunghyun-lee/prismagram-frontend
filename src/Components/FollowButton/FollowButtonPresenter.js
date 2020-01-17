4256f4d6be978f536ca3ecb1831408d1535ad80e
seunghyun <seunghyunleekr@gmail.com>
2020-01-17 16:07:25 +0900

    bug fix

--- src/Components/FollowButton/FollowButtonPresenter.js
+++ src/Components/FollowButton/FollowButtonPresenter.js
@@ -2,5 +2,5 @@ import React from "react";
 import Button from "../Button";
 
 export default ({ isFollowing, onClick }) => (
-    <Button text={isFollowing ? "Unfollow" : "Follow"} on onClick={onClick} />
+    <Button text={isFollowing ? "Unfollow" : "Follow"} onClick={onClick} />
 );
